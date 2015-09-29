ee5300151eca8818deae51b8835ed149417f0429
weiheng <weihengsu3@gmail.com>
2015-09-29 19:48:22 -0400

    Initial commit

---
+++ New folder/js/js_final.js
@@ -0,0 +1,14 @@
+textarea.focus();
+function submit_function(){
+	alert("You have successfully submitted your story! Thanks for your sharing!");
+	var a = document.getElementById("textarea");
+	a.value=null;
+	textarea.focus();
+}
+
+
+
+
+function submit_function2(){
+	alert("You have successfully submitted your contact information! Thank you so much!");
+}
