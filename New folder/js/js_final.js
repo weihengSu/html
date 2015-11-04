009c8993c170838d1a47404f3a73e02584bb478f
weiheng <weihengsu3@gmail.com>
2015-11-03 22:50:16 -0500

    Updated commit

--- New folder/js/js_final.js
+++ New folder/js/js_final.js
@@ -2,13 +2,9 @@ textarea.focus();
 function submit_function(){
 	alert("You have successfully submitted your story! Thanks for your sharing!");
 	var a = document.getElementById("textarea");
-	a.value=null;
+	a.value = null;
 	textarea.focus();
 }
 
 
 
-
-function submit_function2(){
-	alert("You have successfully submitted your contact information! Thank you so much!");
-}
